cab2d033b3005f88662ac1ca82ffe0460cce486f
JacksonKaunismaa <jackkaunis@gmail.com>
2019-08-12 13:52:29 -0400

    finished HTM spatial pooler functionality (will fix the bugs later) (also i messed up git)

--- HTM-basic/htm.py
+++ HTM-basic/htm.py
@@ -1,32 +1,375 @@
 import numpy as np
-import string
-# Let's make an HTM that can learn alphabetical order ie. input size is in [0,25] corresponding to elements, with constant size gaps
-ALPHA_SIZE = 26
-LETTERS = string.ascii_lowercase
-num_to_alpha = {i: LETTERS[i] for i in range(ALPHA_SIZE)}
+import time
+from PIL import Image
+import cv2
+import util_funcs
+np.set_printoptions(precision=3)
+np.random.seed(12)
+NL = "\n"
+TAB = "\t"
 
-def rand_letter():
-    return np.random.randint(0,26,1)[0]
+INPUT_SIZE = 50
+NET_WIDTH = 25
+NET_HEIGHT = 25
+COL_HEIGHT = 4
+NETWORK_SIZE = NET_WIDTH * NET_HEIGHT * COL_HEIGHT
+MAX_SEGMENT_SIZE = 4
+MIN_SEGMENT_SIZE = 2
+NUM_PROXIMAL_INPUTS = 25
+NUM_COLUMNS = NET_HEIGHT * NET_WIDTH
+NUM_OUTPUTS_PER_CELL = 20
 
-def get_gap_data(gap_size, amount):
-    first_letters = np.random.randint(0, ALPHA_SIZE//2, size=(amount,))
-    full_seq = [np.array(list(range(fl,ALPHA_SIZE,gap_size))) for fl in first_letters]
-    #rand_seq = []
-    #for fs in full_seq:
-    #    fs[np.random.randint(2,len(fs)-2)] = rand_letter()
-    #    rand_seq.append(fs)
-    #return rand_seq
-    return full_seq
+MIN_OVERLAP = 3
+MIN_PERM_CONNECTED = 0.5
+INHIBITION_RADIUS = 100    # it wraps circularly around for stastical reasons (although this is nonsensical in normal brains)
+DESIRED_ACTIVITY = 99     # its a winning col if its score is greater than score of 10th greatest column in inhibition radius
+PERMANENCE_INC = 0.01
+BOOSTING_WEIGHT = 0.99    # (=> an input wil decay to having 0.01 significance after 
+BOOST_INC = 0.05
 
-def num_to_let(seqs):
-    if type(seqs) == list:
-        map_func = lambda seq: ''.join(list(map(lambda x: num_to_alpha[x], seq)))
-        return list(map(map_func, seqs))
-    return ''.join(list(map(lambda x: num_to_alpha[x], seq)))
+
+delete_flag = 10
+add_flag = 20
+class ProximalSynapse:
+    def __init__(self, inpt_idx, column=None):
+        self.column = column
+        self.idx = inpt_idx
+        self.activation = 0
+        self.connected = 0
+
+    def activate(self, inpt_idx):
+        if self.connected:    # if it had a permanence value high enough and the input the synapse is linked to is activated, the synapse is activated
+            if self.idx == inpt_idx:
+                self.activation = 1
+        else:
+            self.activation = 0
+
+    def increment_permanence(self):
+        return self.set_permanence(self.permanence + PERMANENCE_INC)
+
+    def decrement_permanence(self):
+        return self.set_permanence(self.permanence - PERMANENCE_INC)
+
+    def set_permanence(self, perm):
+        self.permanence = max(min(1.0, perm), 0.0)
+        if not self.connected and self.permanence > MIN_PERM_CONNECTED:     # if not connected before and should be connected now
+            self.connected = 1
+            return add_flag
+        elif self.connected and self.permanence < MIN_PERM_CONNECTED:      # if connected before and shouldn't be connected now
+            self.connected = 0
+            return delete_flag
+        else:
+            return False
+
+    def __repr__(self):
+        return f"{TAB}ProximalSynapse(link={self.idx},perm={round(self.permanence,3)},act={self.activation},valid={self.connected})"
+
+
+class ProximalSegment:
+    def __init__(self, num_connections, column=None):
+        center = column % INPUT_SIZE
+        self.size = num_connections
+        self.column = column
+        self.idxs = util_funcs.rand_choice_center_fast(INPUT_SIZE, NUM_PROXIMAL_INPUTS, center)      #np.random.choice(INPUT_SIZE, num_connections, replace=False)
+        perms = util_funcs.truncated_normal_perms(center, self.idxs, INPUT_SIZE)
+        self.synapses = [ProximalSynapse(idx, column=self.column) for i, idx in enumerate(self.idxs)]
+        for syn, perm in zip(self.synapses, perms):
+            syn.set_permanence(perm)
+        self.activation = 0
+
+        sorted_synapses = sorted(self.synapses, key=lambda x: x.idx)
+        self.valid_synapses = [x for x in sorted_synapses if x.connected]
+
+    def get_overlap(self, inpt_idxs):
+        self.apply_input(inpt_idxs)
+        overlap = 0
+        for syn in self.synapses:
+            overlap += syn.activation
+        return overlap
+
+    def apply_input(self, inpt_idxs):
+        for inpt_idx in inpt_idxs:
+            for syn in self.synapses:
+                syn.activate(inpt_idx)
+
+    def update_synapses(self):
+        for syn in self.synapses:
+            if syn.activation:                # should always be called after self.apply_input(), then update permanences (if synapses got activated, increase it, else, decrease it)
+                if syn.increment_permanence() == add_flag:    # ie. should be newly added 
+                    self.valid_synapses.insert(util_funcs.binary_insert(self.valid_synapses, syn, key=lambda x: x.idx), syn)
+            else:
+                if syn.decrement_permanence() == delete_flag:
+                    self.valid_synapses.pop(util_funcs.binary_find(self.valid_synapses, syn, key=lambda x: x.idx))
+
+    def get_receptive_field(self):
+        try:
+            return util_funcs.modular_radius(self.valid_synapses, INPUT_SIZE, key=lambda x: x.idx)
+        except IndexError:
+            print("WORTHLESS COLUMN", self)
+            return 0
+
+    def __repr__(self):
+        return f"ProximalSegment(size={self.size},act={self.activation},synapses={NL}{TAB}{(NL+TAB).join([str(syn) for syn in self.synapses])})"
+
+class DistalSynapse:
+    def __init__(self, pre_cell, post_seg):
+        self.pre_cell = pre_cell
+        self.post_seg = post_seg
+        self.permanence = np.random.uniform(low=0.4, high=0.6, size=1)[0]
+        self.activation = 0
+        self.connected = 1 if self.permanence > MIN_PERM_CONNECTED else 0
+
+    def activate(self):
+        if self.connected:
+            if self.pre_cell.activation:
+                self.activation = 1
+        else:
+            self.activation = 0
+
+    def __repr__(self):
+        return f"{TAB}DistalSynapse(pre_loc={self.pre_cell.loc},post_loc={self.post_seg.cell.loc},perm={round(self.permanence,3)},act={self.activation},valid={self.connected})"
+
+class DistalSegment:    # receiver node
+    def __init__(self, post_cell):
+        self.size = 0 #len(pre_connections)
+        self.cell = post_cell
+        self.synapses = []
+        self.activation = 0
+
+    def activate(self):
+        overlap = 0
+        for syn in self.synapses:
+            overlap += syn.activation
+        overlap *= 2
+        if overlap > MIN_OVERLAP:
+            self.activation = 1
+        else:
+            self.activation = 0
+
+    def apply_input(self, idxs):
+        for idx in idxs:
+            if idx in self.idxs:
+                for syn in self.synapses:
+                    syn.activate(idx)
+
+    def link_inputs(self, pre_cell):
+        self.synapses.append(DistalSynapse(pre_cell, self))
+        self.size += 1
+
+    def __repr__(self):
+        return "DistalSegment(size={},act={},synapses=\n\t{})".format(self.size, self.activation, "\n\t".join([str(syn) for syn in self.synapses]))
+
 class Cell:
+    def __init__(self, loc, region, post_cells, column):   # Network randomly provides cells for Cell to be its output
+        self.column = column
+        self.region = region
+        self.post_cells = post_cells
+        self.segments = []
+        self.loc = loc     # this is a 3-vector of (x, y, col_height)
+        self.activation = 0     # can take values 0, 1e-8, 1  to be (inactive, predictive, active)
+        self.ff_act = 0
 
-test = get_gap_data(3,5)
-print(rand_letter())
-print(test)
-print(num_to_let(test))
+    def next_segment(self):
+        for seg in self.segments:
+            if seg.size < MAX_SEGMENT_SIZE:
+                return seg
+        new_seg = DistalSegment(self)
+        self.segments.append(new_seg)
+        return new_seg
+
+    def link_distal(self):
+        for post_loc in self.post_cells:   # for out in outputs
+            post_seg = self.region.cells[post_loc].next_segment()    # for segment to link up to 
+            post_seg.link_inputs(self)     # since this is the outputting cell, it is the pre_cell
+
+    def link_proximal(self, proximal_seg):
+        self.proximal = proximal_seg
+
+    def feed_forward(self, inpt_idxs):
+        self.proximal.apply_input(inpt_idxs)
+        activation, overlap = self.proximal.activate()
+        if activation:
+            self.ff_act = overlap
+        else:
+            self.ff_act = 0
+
+    def prune(self):
+        bad_idxs = []
+        for idx, seg in enumerate(self.segments):
+            if seg.size < MIN_SEGMENT_SIZE:
+                bad_idxs.append(idx)
+        for bad_idx in bad_idxs[::-1]:
+            del self.segments[bad_idx]
+
+    def __repr__(self):
+        return f"Cell(col={self.column},loc={self.loc},outs={self.post_cells},prox_seg={self.proximal},dist_segs={NL}{TAB}{(NL+TAB).join([str(seg) for seg in self.segments])})"
+
+class Column:
+    def __init__(self, cell_list, col_idx, region_ref):
+        self.region_ref = region_ref
+        self.cells = cell_list
+        self.loc = col_idx
+        self.overlap = 0
+        self.boost_factor = 1.0
+        self.activity = 0    # activity should be in [0,1]
+        self.overlap_freq = 0
+
+    def link_proximal(self):
+        proximal_seg = ProximalSegment(NUM_PROXIMAL_INPUTS, column=self.loc)
+        self.proximal = proximal_seg
+        for cell in self.cells:
+            cell.link_proximal(proximal_seg)
+
+    def overlap_input(self, inpt_idxs):
+        overlap = self.proximal.get_overlap(inpt_idxs)
+        if overlap < MIN_OVERLAP:
+            self.overlap = 0
+            self.overlap_freq *= BOOSTING_WEIGHT
+        else:
+            self.overlap_freq = self.overlap_freq*BOOSTING_WEIGHT + (1-BOOSTING_WEIGHT)
+            self.overlap = overlap * self.boost_factor
+
+    def activate(self):
+        for cell in self.cells:
+            cell.activation = 1
+            self.activity = self.activity*BOOSTING_WEIGHT + (1-BOOSTING_WEIGHT) * 1
+        self.proximal.update_synapses()
+
+    def not_activate(self):
+        self.activity = self.activity*BOOSTING_WEIGHT
+
+    def inhibit(self, sliding_window, sorted_window, add_val):
+        del_val = sliding_window.pop(0)
+        sliding_window.append(add_val)
+        sorted_window.remove(del_val)
+        sorted_window.insert(util_funcs.binary_insert(sorted_window, add_val), add_val)
+        if self.overlap and self.overlap >= sorted_window[DESIRED_ACTIVITY]:
+            self.activate()
+        else:
+            self.not_activate()
+
+    def boost(self, sliding_window, sorted_window, add_val):
+        del_val = sliding_window.pop(0)
+        sliding_window.append(add_val)
+        sorted_window.remove(del_val)
+        sorted_window.insert(util_funcs.binary_insert(sorted_window, add_val), add_val)
+
+        min_activity = sorted_window[0] * 0.01
+        if self.activity >= min_activity:
+            self.boost_factor = 1.0
+        else:
+            self.boost_factor += BOOST_INC
+
+        if self.overlap_freq < min_activity:
+            for syn in self.proximal.synapses:
+                syn.permanence += 0.1 * MIN_PERM_CONNECTED
+
+    def get_receptive_field(self):
+        return self.proximal.get_receptive_field()
+
+    def __repr__(self):
+        return f"Column(loc={self.loc}, cells={[x.loc for x in self.cells]}, overlap={self.overlap}, proximal_seg={self.proximal})"
+
+class Region:
+    def __init__(self):
+        #self.grid = grid
+        self.linked = False
+        self.cells = np.empty(NETWORK_SIZE, dtype=Cell)
+        full_neighbourhood = np.arange(NETWORK_SIZE)
+        for lin_loc in range(NETWORK_SIZE):  #self.grid is shape (HEIGHT*WIDTH*COL_HEIGHT, 3) so you do like grid[523]->(5, 5, 3) [523//(25*4), (523%100)//4, 523%4]
+            neighbourhood = np.concatenate((full_neighbourhood[:lin_loc], full_neighbourhood[lin_loc+1:]), 0)
+            post_cells = np.random.choice(neighbourhood, size=(NUM_OUTPUTS_PER_CELL), replace=True)
+            self.cells[lin_loc] = Cell(lin_loc, self, post_cells, lin_loc//4)
+
+        self.link()
+        self.prune()
+
+        self.inputs_to_cells = None     # dictionary of which inputs lead to which cells  {input_idx: [cell_list]}  #TODO: implememnt this
+        cols_np = self.cells.reshape(NUM_COLUMNS, COL_HEIGHT)
+        self.cols = np.empty(NUM_COLUMNS, dtype=Column)
+        for i, col_np in enumerate(cols_np):
+            self.cols[i] = Column(col_np, i, self)
+        self.link_cols()
+
+    def visualize(self):
+        reshaped = self.cells.reshape(NET_WIDTH, NET_HEIGHT, COL_HEIGHT).swapaxes(0,-1)
+        for col_layer in reshaped:
+            img = np.zeros_like(col_layer).astype(np.uint8)
+            for x,row in enumerate(col_layer):
+                for y,cell in enumerate(row):
+                    if cell.activation:
+                        if cell.activation == 1:
+                            img[x,y] = 255
+                        else:
+                            img[x,y] = 128
+            #img_resize = cv2.resize(img, (1024, 1024), interpolation=cv2.INTER_NEAREST)
+            pil_img = Image.fromarray(img, "L")
+            #pil_img = Image.fromarray(img_resize, "L")
+            pil_img.show()
+
+
+    def apply_input(self, idxs):
+        for col in self.cols:
+            col.overlap_input(idxs)
+
+    def inhibit(self):
+        sliding_window = [x.overlap for x in np.take(self.cols, range(-INHIBITION_RADIUS//2, INHIBITION_RADIUS//2))]
+        sorted_window = list(sorted(sliding_window))
+        for loc, col in enumerate(self.cols):
+            add_val = self.cols[(loc+INHIBITION_RADIUS//2)%len(self.cols)].overlap
+            col.inhibit(sliding_window, sorted_window, add_val)
+
+    def boost(self):
+        global INHIBITION_RADIUS
+        sliding_window = [x.activity for x in np.take(self.cols, range(-INHIBITION_RADIUS//2, INHIBITION_RADIUS//2))]
+        sorted_window = list(sorted(sliding_window))
+        for loc, col in enumerate(self.cols):
+            add_val = self.cols[(loc+INHIBITION_RADIUS//2)%len(self.cols)].activity
+            col.boost(sliding_window, sorted_window, add_val)
+        INHIBITION_RADIUS = int(self.average_receptive_field())
+
+    def average_receptive_field(self):
+        total = 0
+        for col in self.cols:
+            total += col.get_receptive_field()
+        return float(total) / NUM_COLUMNS
+
+
+    def link_cols(self):
+        for col in self.cols:
+            col.link_proximal()
+
+    def link(self):
+        if not self.linked:
+            for cell in self.cells:
+                cell.link_distal()
+            self.linked = True
+
+    def prune(self):
+        for cell in self.cells:
+            cell.prune()
+
+def main():
+    #grid_region = np.mgrid[0:NET_WIDTH, 0:NET_HEIGHT, 0:COL_HEIGHT].reshape(3,-1).T#.swapaxes(0,-1).swapaxes(0,2).swapaxes(0,1)
+    cell_region = Region()
+    print(cell_region.cells[49])
+    print(f"{'#'*100}\n"*3,end='')
+    print(cell_region.cols[12])
+    print(f"{'#'*100}\n"*3,end='')
+    #cell_region.cells[123].activation = 1
+    #cell_region.cells[124].activation = 1
+    #cell_region.cells[125].activation = 1
+    cell_region.apply_input([25,11,10,8,2])
+    #cell_region.visualize()
+    #input()
+    cell_region.inhibit()
+    #cell_region.visualize()
+    #input()
+    cell_region.boost()
+    print(INHIBITION_RADIUS)
+    cell_region.visualize()
+
+
+if __name__ == "__main__":
+    main()
 
---
+++ HTM-basic/util_funcs.py
@@ -0,0 +1,175 @@
+import numpy as np
+import time
+from collections import Counter
+
+def rand_choice_center(a, size, center):
+    # DONT USE THIS (too slow)
+    choices = np.random.normal(center, a/6, size=size).astype(np.uint8) % a
+    while np.unique(choices).shape != size:
+        choices = np.random.normal(center, a/6, size=size).astype(np.uint8) % a
+    return choices
+
+def rand_choice_center_fast(a, size, center):
+    def sub_func():
+        double_choices = np.random.normal(center, a/6, size=(2*size,)).astype(np.uint8) % a
+        used = set()
+        actual_choices = np.empty(size)
+        current = 0
+        for c in double_choices:
+            if current != size:
+                if c not in used:
+                    used.add(c)
+                    actual_choices[current] = c
+                    current += 1
+            else:
+                break
+        return actual_choices, current
+    choices, actual_size =  sub_func()
+    while actual_size != size:
+        choices, actual_size =  sub_func()
+    return choices
+
+def truncated_normal_perms(center, inpts, inpt_size):
+    return np.clip(np.random.normal(0.52-0.05*mod_dist(center, inpts, inpt_size)/float(inpt_size), 1e-2), 0.3, 0.7)
+
+def rand_choice_center_bias(a, size, center):
+    # DONT USE THIS (gives biased results [there is replacement])
+    choices = np.random.normal(center, a/6, size=size).astype(np.uint8) % a
+    return choices
+
+def mod_dist(a, b, c):
+    diffs = abs(a-b)
+    return np.where(diffs < c//2, diffs, c - diffs)
+
+
+def _binary_insert(l, lo, hi, val, old_mid):
+    mid = (lo+hi)//2
+    if mid == old_mid:
+        if val < l[lo]:
+            return mid-1
+        elif val > l[hi]:
+            return mid+1
+        else:
+            return mid
+#    print("lo", l[lo], "hi", l[hi], "val", val, "mid", l[mid])
+    if val <= l[mid]:
+ #       print("first path")
+        return _binary_insert(l, lo, mid, val, mid)
+    else:
+  #      print("second path")
+        return _binary_insert(l, mid, hi, val, mid)
+
+def binary_insert(l , val, key=None):
+    if key is not None:
+        l_actual = list(map(key, l))
+        try:
+            val = key(val)
+        except Exception as e:
+            pass
+        return _binary_insert(l_actual, 0, len(l)-1, val, -1) + 1
+    return _binary_insert(l, 0, len(l)-1, val, -1) + 1
+
+def binary_find(l, val, key=None):
+    # use this one if you want to delete from the sorted array
+    res = binary_insert(l, val, key=key)
+    try:
+        if res == 1 and key(val) <= key(l[0]):     # this is the only difference between binary insert and binary find
+            return 0
+    except Exception as e:
+        if res == 1 and val <= key(l[0]):
+            return 0
+    return res
+
+def modular_radius(vals, mod, key=None):
+    #sorted_vals = sorted(vals)
+    #sorted_vals.append(sorted_vals[0])
+    if key is not None:
+        vals_actual = list(map(key, vals))
+        vals_actual.append(key(vals[0]))
+    else:
+        vals_actual = vals
+        vals_actual.append(vals[0])
+    #print(vals_actual)
+    lowest = mod
+    for x,y in zip(vals_actual, vals_actual[1:]):
+     #   print(x,y, "=>", mod - (y-x)%mod)
+        lowest = min(lowest, mod - (y-x)%mod)
+    return lowest
+
+def main():
+    np.set_printoptions(precision=3)
+    #starter = [5, 123, -23, 4, 0, 4,2, 13, 5, 2, 4, 123,5,3,3,3,3,523,43,212, 12, 13, 13, 13, 14, 15, 16]
+    #starter.sort()
+    #print(starter)
+    #result = binary_insert(starter, 5)
+    #starter.insert(result, 5)
+    #result = binary_insert(starter, 27)
+    #starter.insert(result, 27)
+    #result = binary_insert(starter, -500)
+    #starter.insert(result, -500)
+    #result = binary_insert(starter, 5000)
+    #starter.insert(result, 5000)
+    #result = binary_insert(starter, 50)
+    #starter.insert(result, 50)
+    #print(starter)
+    N = 50000
+
+
+    #print(mod_dist(1,2,50), '1,2')
+    #print(mod_dist(24,27,50), '24,27')
+    #print(mod_dist(25,26,50), '25,26')
+    #print(mod_dist(24,25,50), '24,25')
+    #print(mod_dist(4,29,50), '4,29')
+    #print(mod_dist(1,10,50), '0,10')
+    #print(mod_dist(0,50,50), '0,50')
+    #print(mod_dist(0,49,50), '0,49')
+    #print(mod_dist(10,1,50), '10,1')
+    #print(mod_dist(50,0,50), '50,0')
+    #print(mod_dist(49,0,50), '49,0')
+    #quit()
+    #start = time.time()
+    #for _ in range(N):
+    #    rand_choice_center(50, (15,), 12)
+    #print((time.time() - start))
+
+    #start = time.time()
+    #for _ in range(N):
+    #    rand_choice_center_bias(50, (15,), 12)
+    #t1 = ((time.time() - start))
+
+    #start = time.time()
+    #for _ in range(N):
+    #    rand_choice_center_fast(50, 15, 12)
+    #t2 = ((time.time() - start))
+    #print(t1)
+    #print(t2)
+    #print(t2/t1)
+    total_conn = 0
+    total_discon = 0
+    distr = Counter()
+    total_distr = Counter()
+    for _ in range(N):
+        res = rand_choice_center_fast(50, 15, 0)
+        perms = truncated_normal_perms(0, res, 50)
+        amounts = np.where(perms > 0.5, 0, 1).sum()
+
+        for count, perm in zip(res, perms):
+            if perm > 0.5:
+                distr[int(count)] += 1
+            total_distr[int(count)] += 1
+
+        total_discon += amounts
+        total_conn += (15 - amounts)
+    res = rand_choice_center_fast(50, 15, 0)
+    perms = truncated_normal_perms(0, res, 50)
+    print(res)
+    print(mod_dist(0, res, 50))
+    print(perms)
+    print(np.where(perms > 0.5, 0, 1).sum())
+    print(total_conn)
+    print(total_discon)
+    print(distr, ">0.5 coutns")
+    print(total_distr, "potential synapse coutns")
+
+if __name__ == "__main__":
+    main()
